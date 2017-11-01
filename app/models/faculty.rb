6a6b35f0c943f9a88cb68ee1957cb4906089d9a3
manish15597 <bmanish15597@gmail.com>
2017-11-02 02:21:02 +0530

    On delete cascade

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,5 +1,5 @@
 class Faculty < ApplicationRecord
   belongs_to :users, class_name: User
   self.primary_key = :users_id;
-  has_many :courses, foreign_key: :faculty_id,class_name: "Course"
+  has_many :courses, foreign_key: :faculty_id,class_name: "Course", :dependent => :delete_all
 end
